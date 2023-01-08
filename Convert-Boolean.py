61241eeb9250d5cc58437037f9880857b96b2937
yousef-dev1 <ysherian@gmail.com>
2023-01-08 13:30:36 -0600

    Solution

---
+++ Convert-Boolean.py
@@ -0,0 +1,16 @@
+# Implement a function which convert the given boolean value into its string representation.
+
+# Note: Only valid inputs will be given.
+
+def boolean_to_string(b):
+    #your code here
+    if b:
+        return 'True'
+    else:
+        return 'False'
+
+# Other solutions:
+
+#def boolean_to_string(b):
+#    return str(b)
+
